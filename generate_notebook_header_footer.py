2d9d1a48101d71e3ddf3e99e9e2f6d603b4ea0c0
flothesof <florian.s.lebourdais@gmail.com>
2019-07-03 11:08:03 +0200

    updated template for footer

--- generate_notebook_header_footer.py
+++ generate_notebook_header_footer.py
@@ -18,9 +18,9 @@ def filename2url(filename):
 if __name__ == "__main__":
     try:    
         nb_name = sys.argv[1]
-        print("""*This post was entirely written using the IPython notebook. Its content is BSD-licensed. You can see a static view or download this notebook with the help of nbviewer at [{0}]({1}).*""".format(nb_name, filename2url(nb_name)))
+        print("""*This post was entirely written using the Jupyter Notebook. Its content is BSD-licensed. You can see a static view or download this notebook with the help of nbviewer at [{0}]({1}).*""".format(nb_name, filename2url(nb_name)))
         
-        print("""*Ce billet a été écrit à l'aide d'un notebook Jupyter. Son contenu est sous licence BSD. Une vue statique de ce notebook peut être consultée et téléchargée ici : [{0}]({1}).*""".format(nb_name, filename2url(nb_name)))
+        print("""*Ce billet a été écrit à l'aide d'un Notebook Jupyter. Son contenu est sous licence BSD. Une vue statique de ce notebook peut être consultée et téléchargée ici : [{0}]({1}).*""".format(nb_name, filename2url(nb_name)))
         
     except:
         print("Usage: python generate_notebook_header_footer.py notebook_name")
